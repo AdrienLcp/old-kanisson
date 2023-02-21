7873dbbe8233ffc398744ea5a08b59bd04ff1b91
Adrien <adrienlcp@gmail.com>
2023-02-21 14:03:15 +0100

    change domain url

--- api/api.ts
+++ api/api.ts
@@ -1,3 +1,3 @@
-export const api = "http://localhost:3000/api/";
+export const api = "https://www.kanisson.com/api/";
 
 export const youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=";
