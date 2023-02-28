3e48ef3a734f1e672ab36876df74c041999b5ca9
Adrien <adrienlcp@gmail.com>
2023-02-28 15:13:17 +0100

    fix api url

--- api/api.ts
+++ api/api.ts
@@ -1,4 +1,4 @@
-export const api = "http://localhost:3000/api/";
-// export const api = "https://www.kanisson.com/api";
+// export const api = "http://localhost:3000/api/";
+export const api = "https://www.kanisson.com/api";
 
 export const youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=";
